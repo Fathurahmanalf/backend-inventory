66c277d8b2c0acd7fbf353440cbb89d3a18378b7
Fathurahmanalf <113759542+Fathurahmanalf@users.noreply.github.com>
2023-11-21 21:54:23 +0700

    CRUD Version Not Fix

--- routes/handlers/products/id/put.js
+++ routes/handlers/products/id/put.js
@@ -1,8 +1,10 @@
-const { Product } = require("../../../../models")
+const { Product, Version, Log } = require("../../../../models");
 
 module.exports = async (req, res) => {
     const { productId } = req.params;
-    const product = await Product.findByPk(productId);
+    const product = await Product.findByPk(productId, {
+        include: [Version, Log],
+    });
 
     if (!product)
         return res.status(404).json({
@@ -10,7 +12,32 @@ module.exports = async (req, res) => {
             message: "Product not found"
         });
 
-    await product.update(req.body);
+    try {
+        // Update data di tabel Product dan Version
+        await product.update({
+            productName: req.body.productName,
+        });
 
-    return res.json(product)
+        // Update data di tabel Version
+        const version = product.Version; 
+        await version.update({
+            versionName: req.body.versionId,
+        });
+
+        // Update data di tabel Log
+        const log = product.Logs[0]; 
+        await log.update({
+            qty: req.body.qty,
+        });
+
+
+        return res.json(product);
+    } catch (error) {
+        
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error",
+        });
+    }
 }
